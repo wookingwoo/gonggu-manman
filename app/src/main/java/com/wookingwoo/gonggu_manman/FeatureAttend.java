9ced964f8f87ca97cf8dd07668b874b7b9dbd283
S3-20181650 <39684697+S3-20181650@users.noreply.github.com>
2021-11-07 23:56:17 +0900

    base code

---
+++ app/src/main/java/com/wookingwoo/gonggu_manman/FeatureAttend.java
@@ -0,0 +1,12 @@
+package com.wookingwoo.gonggu_manman;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeatureAttend extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feature_attend);
+    }
+}
