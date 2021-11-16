e654b1fa49b0d85261e56b4b8f42c32c99e361cb
S3-20181650 <39684697+S3-20181650@users.noreply.github.com>
2021-11-16 22:47:45 +0900

    UI 구현, 기능 x

--- app/src/main/java/com/wookingwoo/gonggu_manman/FeatureAttend.java
+++ app/src/main/java/com/wookingwoo/gonggu_manman/FeatureAttend.java
@@ -2,11 +2,36 @@ package com.wookingwoo.gonggu_manman;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.animation.Animator;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.ScaleAnimation;
+import android.widget.Button;
+import android.widget.CompoundButton;
 
 public class FeatureAttend extends AppCompatActivity {
+    ScaleAnimation scaleAnimation;
+    BounceInterpolator bounceInterpolator;//애니메이션이 일어나는 동안의 회수, 속도를 조절하거나 시작과 종료시의 효과를 추가 할 수 있다
+    CompoundButton button_favorite;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feature_attend);
+
+        scaleAnimation = new ScaleAnimation(0.7f, 1.0f, 0.7f, 1.0f, Animation.RELATIVE_TO_SELF, 0.7f, Animation.RELATIVE_TO_SELF, 0.7f);
+
+        scaleAnimation.setDuration(500);
+        bounceInterpolator = new BounceInterpolator();
+        scaleAnimation.setInterpolator(bounceInterpolator);
+
+        button_favorite = findViewById(R.id.button);
+
+        button_favorite.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                compoundButton.startAnimation(scaleAnimation);
+            }
+        });
     }
 }
